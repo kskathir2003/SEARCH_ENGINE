80e14bba20f4dd8187c4e35fd181fbf94269a0eb
KATHIR K S <108514360+kskathir2003@users.noreply.github.com>
2023-09-06 17:43:45 +0530

    Delete Search Engine directory

--- Search Engine/crawlAndSave.js
+++
@@ -1,9 +0,0 @@
-const crawl = require('./crawl')
-const save = require('.save')
-
-async function crawlAndSave(domain, progressData, saveProgress, browser) {
-    const { htmlContent, textContent } = await crawl(domain, progressData, browser);
-    save(domain, progressData, htmlContent, textContent, saveProgress);
-}
-
-module.exports = crawlAndSave;
--- Search Engine/saveProgess.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-function saveProgress(progressData) {
-    fs.writeFileSync(path.join(__dirname, 'progress.json'), JSON.stringify(progressData, null, 2));
-}
-
-module.exports = saveProgress;
