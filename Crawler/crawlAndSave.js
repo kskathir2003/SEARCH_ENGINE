6adb8b6ae950a1559a53649aad68edf2c7806ffe
KATHIR K S <108514360+kskathir2003@users.noreply.github.com>
2023-09-06 18:25:24 +0530

    Update crawlAndSave.js

--- Crawler/crawlAndSave.js
+++ Crawler/crawlAndSave.js
@@ -1,5 +1,5 @@
 const crawl = require('./crawl')
-const save = require('.save')
+const save = require('./save')
 
 async function crawlAndSave(domain, progressData, saveProgress, browser) {
     const { htmlContent, textContent } = await crawl(domain, progressData, browser);
